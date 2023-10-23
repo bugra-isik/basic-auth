d348f7df9f41f16916a7198459f255dab7e1710b
Buğra <librouse98@gmail.com>
2023-10-24 00:06:35 +0300

    v1.6.8.1

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,21 +1,23 @@
-import connectMongoDB from "@/config/dbConn";
-import mongoose, { connection } from "mongoose";
+import mongoose from "mongoose";
 
 export async function POST(req: Request, res: Response) {
   let deneme = "bırt";
-  await mongoose
-    .connect(
-      "mongodb+srv://librouse98:ze1dodp9JXHfTGwB@cluster0.4qjwa0b.mongodb.net/?retryWrites=true&w=majority",
+
+  try {
+    await mongoose.connect(
+      "mongodb+srv://librouse98:ze1dodp9JXHfTGwB@cluster0.4qjwa0b.mongodb.net/UserDB",
       {
         dbName: "UserDB",
       },
-    )
-    .then((e) => {
-      console.log(e);
-      deneme = "tımım";
-    })
-    .catch((e) => console.error(e));
+    );
 
-  await mongoose.connection.close();
+    console.log("Bağlantı başarılı");
+    deneme = "tımım";
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await mongoose.connection.close();
+  }
+
   return Response.json(deneme);
 }
